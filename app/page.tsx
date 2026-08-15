b6f4cf47e43bff78b42ae4de842460d254ca0dab
Luiza Gosling <luiza@nasacada.com.br>
2026-08-14 21:49:12 -0300

    Ativa lista de espera após vagas esgotadas

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ const analyticsAvailable = Boolean(process.env.NEXT_PUBLIC_GA_ID);
 const presencialUrl = "https://www.oficina.cc/event-details/queimas-poeticas-com-kuara-ceramicas-2";
 const socialSpotsUrl = "https://forms.gle/H28ag11q2wUpd4Zr7";
 const freeClassFormOpensAt = Date.parse("2026-08-03T00:00:00-03:00");
-const waitlistFormOpensAt = Date.parse("2026-08-15T00:00:00-03:00");
+const waitlistFormOpensAt = Date.parse("2026-08-14T00:00:00-03:00");
 const turnstileSiteKey = "0x4AAAAAAEFzz82dfHPlJVVM";
 
 type TurnstileApi = {
